5b6372bae2a308afde30e83c979ee1468a8f3012
shihe110 <github110@126.com>
2021-02-07 16:33:34 +0800

    about jsl-etf fix bug

--- sgd-jsl/src/main/java/com/shihe/task/ETFScheduleTask.java
+++ sgd-jsl/src/main/java/com/shihe/task/ETFScheduleTask.java
@@ -31,7 +31,7 @@ public class ETFScheduleTask {
      * 每日etf数据-定时器
      * @throws IOException
      */
-    @Scheduled(cron = "0 17 16 ? * MON-SUN")
+    @Scheduled(cron = "0 33 16 ? * MON-SUN")
     private void configureTasks() throws IOException {
         String url = "https://www.jisilu.cn/data/etf/etf_list/";
         String jsonstr = etfParser.getCall(url);
