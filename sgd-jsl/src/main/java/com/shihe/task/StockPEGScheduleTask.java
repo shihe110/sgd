882eff72fb4cefadf2844556ee5013aed924ec27
shihe110 <github110@126.com>
2021-03-23 20:43:13 +0800

    about jsl-stock java

--- sgd-jsl/src/main/java/com/shihe/task/StockPEGScheduleTask.java
+++ sgd-jsl/src/main/java/com/shihe/task/StockPEGScheduleTask.java
@@ -37,6 +37,20 @@ public class StockPEGScheduleTask {
     @Autowired
     private ISgdStockHistoryService iSgdStockHistoryService;
 
+    @Scheduled(cron = "0 29 15 ? * MON-FRI")
+    private void configureJslTasks() throws IOException {
+        QueryWrapper<SgdStockOf> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_id",2);
+        List<SgdStockOf> list = iSgdStockOfService.list(wrapper);
+        for (SgdStockOf stockOf : list) {
+
+
+            String code = stockOf.getsCode();
+            String peg = getPEG(code);
+        }
+    }
+
+
     /**
      * 每日自选股票历史数据-定时器
      *
@@ -45,7 +59,8 @@ public class StockPEGScheduleTask {
     @Scheduled(cron = "0 29 15 ? * MON-FRI")
     private void configureTasks() throws IOException {
         QueryWrapper<SgdStockOf> wrapper = new QueryWrapper<>();
-        wrapper.ne("s_market","-");
+        wrapper.eq("user_id",2);
+//        wrapper.ne("s_market","-");
         List<SgdStockOf> list = iSgdStockOfService.list(wrapper);
         for (SgdStockOf stockOf : list) {
             String code = stockOf.getsCode();
@@ -84,6 +99,29 @@ public class StockPEGScheduleTask {
 
         return stockHistory;
     }
+    public String getJsl(String code) throws IOException {
+        String url = "https://www.jisilu.cn/data/stock/" + code;
+        Document doc = Jsoup.parse(new URL(url), 30000);
+        Elements elementsByTag = doc.getElementById("stock_detail").getElementsByTag("tbody");
+        Elements tr = elementsByTag.select("tr").last().children();
+        // 5年增长率
+        String rt = tr.get(3).children().text();
+        rt = rt.substring(0, rt.length() - 1);
+
+        Elements item_sub = doc.getElementsByClass("item_row");
+        Elements children = item_sub.get(0).children();
+        Element element = children.get(0).children().get(0);
+        Elements div = element.getElementsByTag("div");
+        // 当前pe
+        String pe = div.get(2).select("span").text();
+        System.out.println(pe);
+        String text = div.get(5).text();
+        // 平均pe
+        String avg = text.substring(4, text.length() - 1).trim();
+        // peg pe/(rt*100)
+        double peg = Double.valueOf(pe) / Double.valueOf(rt);
+        return String.valueOf(peg);
+    }
 
     public String getPEG(String code) throws IOException {
         String url = "https://www.jisilu.cn/data/stock/" + code;
@@ -113,10 +151,39 @@ public class StockPEGScheduleTask {
         String url = "https://www.jisilu.cn/data/stock/000001";
         Document doc = Jsoup.parse(new URL(url), 30000);
         Elements elementsByTag = doc.getElementById("stock_detail").getElementsByTag("tbody");
+        Elements tr = elementsByTag.select("tr");
+        Elements td1 = tr.get(0).getElementsByTag("td");
+        String t0 = td1.get(0).text();
+        // 现价 涨幅  市值  总股本
+        String price = td1.get(1).text().substring(2).trim();
+        String incremental = td1.get(2).text().substring(2).trim().substring(0,td1.get(2).text().substring(2).trim().length()-1);
+        String marketVal = td1.get(3).text().substring(2).trim().substring(0,td1.get(3).text().substring(2).trim().length()-1);
+        String totalStock = td1.get(4).text().substring(3).trim().substring(0,td1.get(4).text().substring(3).trim().length()-2);
+
+        Elements td2 = tr.get(1).getElementsByTag("td");
+        // 股息率
+        String stockRt = td2.get(1).text().substring(6).trim().substring(0,td2.get(1).text().substring(6).trim().length()-1);
+        // 波动率
+        String bodong = td2.get(2).text().substring(3).trim();
+        // 有息负债率
+        String yxfzRt = td2.get(3).text().substring(5).trim().substring(0,td2.get(3).text().substring(5).trim().length()-1);
+        // 股票质押比例
+        String zhiya = td2.get(4).text().substring(6).trim();
+
+        Elements td3 = tr.get(2).getElementsByTag("td");
+        // 5年平均roe 	5年营收增长率 2.75%	5年利润增长率 7.51%	净利同比增长 -55.38%
+        String fiveroe = td3.get(1).text().substring(7).trim().substring(0,td3.get(1).text().substring(7).trim().length()-1);
+        String fiveIncre = td3.get(2).text().substring(7).trim().substring(0,td3.get(2).text().substring(7).trim().length()-1);
+        String fiveLrzz = td3.get(3).text().substring(7).trim().substring(0,td3.get(3).text().substring(7).trim().length()-1);
+        String fiveLrl = td3.get(4).text().substring(6).trim().substring(0,td3.get(4).text().substring(6).trim().length()-1);
+
+
+        System.out.println(elementsByTag.html());
 //        System.out.println(elementsByTag.html());
-        Elements tr = elementsByTag.select("tr").last().children();
+        Elements trx = elementsByTag.select("tr").last().children();
+
         // 5年增长率
-        String rt = tr.get(3).children().text();
+        String rt = trx.get(3).children().text();
         rt = rt.substring(0, rt.length() - 1);
 
 
